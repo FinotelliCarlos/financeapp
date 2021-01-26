d15bac8f1e0090f5ad6b159429dde3be915e86f4
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-01-26 11:24:12 -0300

    insert function open and close modal

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,11 +1,15 @@
-Modal = {
+const Modal = {
     open(){
         // Abrir Modal
         // Adicionar class active ao modal
-        alert('Funciona')
+        document.querySelector('.modal-overlay')
+        .classList.add('active')
+
     },
     close(){
         // Fechar Modal
         // Remover classe active do modal
+        document.querySelector('.modal-overlay')
+        .classList.remove('active')
     }
 }
