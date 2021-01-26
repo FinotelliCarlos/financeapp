79f24360116acc5bbea0121284dcb66a6119efb6
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-01-26 10:37:39 -0300

    init script js in modal

---
+++ public/scripts/modal.js
@@ -0,0 +1,11 @@
+Modal = {
+    open(){
+        // Abrir Modal
+        // Adicionar class active ao modal
+        alert('Funciona')
+    },
+    close(){
+        // Fechar Modal
+        // Remover classe active do modal
+    }
+}
